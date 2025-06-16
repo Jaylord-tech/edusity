eb2becc9db11f12cf013790f0a6fc7f2c61fd6ba
Jaytech <oyediran904@gmail.com>
2025-06-16 11:32:23 +0100

    fix: file re-sync issue with assets folder

--- website/my-react-app/src/Button.jsx
+++
@@ -1,9 +0,0 @@
-
-function Button(){
-
-const clickHandle = (e) => e.target.textContent = "OUCH !";
-
-    return(<button onClick={(e)=>clickHandle(e)}>CLICK ME</button>);
-}
-   
-export default Button
--- website/my-react-app/src/Profilepix.jsx
+++
@@ -1,9 +0,0 @@
-
-function Profilepix (){
-
-const imageUrl = './src/assets/images.jpg';
-const image =(e)=>e.target.style.display = 'none';
-
-return(<img onClick={(e) => image (e) } src = {imageUrl}></img>);
-}
-export default Profilepix
